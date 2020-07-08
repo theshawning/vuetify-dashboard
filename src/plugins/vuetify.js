07a9b30565e634907aae0dac78d6c2c1ab1890ee
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-07-08 16:20:11 +0100

    basic theme customization enabled dark theme toggling with button in navbar

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#41b883'
+      },
+      dark: {
+        primary: '#34495e'
+        // , anchor: 'fff'
+        // how to set the a-tag color
+      }
+    }
+  }
 });
