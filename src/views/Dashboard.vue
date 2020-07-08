a74bfe5432d0e1f9df439fa079c5507b54fab077
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-07-08 14:44:53 +0100

    added new sales data and graphs to Dashboard.vue page edited information in Dashboard to use Vuetify grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,117 +1,76 @@
 <template>
-<div>
-  <h1 class="mt-4 mx-2">Dashboard</h1>
-  <v-data-table
-    :headers="headers"
-    :items="employees"
-    :items-per-page="5"
-    class="elevation-1 mt-2 mx-2"
-    @click:row="selectRow"
-    multi-sort="true"
-  ></v-data-table>
-  <v-snackbar v-model="snackbar">
-    You have selected {{ currentPerson }}, {{ currentPersonTitle }}
-    <v-btn
-      color="pink"  
-      text
-      @click="snackbar = false"
-    >
-      Close
-    </v-btn>
-  </v-snackbar>
-</div>
+  <v-container>
+    <h1>Dashboard</h1>
+    <v-row>
+      <v-col v-for="sale in sales" :key="`${sale.title}`">
+        <SalesGraph :sale="sale" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col v-for="statistic in statistics" :key="`${statistic.title}`" >
+        <StatisticCard :statistic="statistic" />        
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />       
+      </v-col>
+      <v-col cols="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+
+    <v-snackbar v-model="snackbar">
+      You have selected {{ selectedEmployee.name }},
+      {{ selectedEmployee.title }}
+      <v-btn color="pink" text @click="snackbar = false">
+        Close
+      </v-btn>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
+import EmployeesTable from "../components/EmployeesTable";
+import EventTimeline from "../components/EventTimeline";
+import SalesGraph from "../components/SalesGraph";
+import StatisticCard from "../components/StatisticCard";
+
+import employeesData from "../data/employees.json";
+import timelineData from "../data/timeline.json";
+import salesData from "../data/sales.json";
+import statisticsData from "../data/statistics.json";
+
 export default {
+  name: "DashboardPage",
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard
+  },
   data() {
     return {
-      currentPerson: "",
-      currentPersonTitle: "",
+      employees: employeesData,
+      sales: salesData,
+      selectedEmployee: {
+        name: "",
+        title: ""
+      },
       snackbar: false,
-      headers: [
-        {
-          text: "Employee Name",
-          align: "start",
-          value: "name"
-        },
-        { text: "Title", value: "title" },
-        { text: "ID Number", value: "id" },
-        { text: "Salary ($)", value: "salary" }
-      ],
-      employees: [
-        {
-          id: 12875,
-          name: "Dwight Schrute",
-          title: "Assistant to the Regional Manager",
-          salary: 65000
-        },
-        {
-          id: 14094,
-          name: "Kelly Kapoor",
-          title: "Customer Service Representative",
-          salary: 40000
-        },
-        {
-          id: 12894,
-          name: "Darryl Philbin",
-          title: "Warehouse Foreman",
-          salary: 68000
-        },
-        {
-          id: 13893,
-          name: "Angela Martin",
-          title: "Accountant",
-          salary: 54000
-        },
-        {
-          id: 12943,
-          name: "Phyllis Vance",
-          title: "Saleswoman",
-          salary: 65000
-        },
-        {
-          id: 19523,
-          name: "Karen Filippeli",
-          title: "Saleswoman",
-          salary: 65000
-        },
-        {
-          id: 18349,
-          name: "Toby Flenderson",
-          title: "Human Resources Representative",
-          salary: 60000
-        },
-        {
-          id: 11853,
-          name: "Michael Scott",
-          title: "Regional Manager",
-          salary: 70000
-        },
-        {
-          id: 14821,
-          name: "Pam Beasley",
-          title: "Secretary",
-          salary: 45000
-        },
-        {
-          id: 15834,
-          name: "Jim Halpert",
-          title: "Salesman",
-          salary: 65000
-        }
-      ]
+      statistics: statisticsData,
+      timeline: timelineData
     };
   },
   methods: {
-    selectRow(event) {
+    setEmployee(event) {
       this.snackbar = true;
-      this.currentPerson = event.name;
-      this.currentPersonTitle = event.title;
+      this.selectedEmployee.name = event.name;
+      this.selectedEmployee.title = event.title;
     }
   }
 };
 </script>
-
-<style scoped>
-</style>
